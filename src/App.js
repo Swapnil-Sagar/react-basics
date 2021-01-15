70c4491a8a03b10b15c2ab9dee22076329a23765
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-01-15 17:59:14 +0530

    Forwarding Refs

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
+import FRParentInput from "./components/FRParentInput";
 
 function App() {
   return (
     <div className="App">
-      <FocusInput />
-      {/*<RefsDemo />
+      <FRParentInput />
+      {/* <FocusInput />
+      <RefsDemo />
       <ParentComp />
       <Table />
       <FragmentDemo />
