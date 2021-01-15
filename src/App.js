fbe9df07646704cd69a0a1b8b94b1c102cee24c1
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-01-15 17:40:15 +0530

    Refs in child

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ import Table from "./components/Table";
 import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 function App() {
   return (
     <div className="App">
-      <RefsDemo />
-      {/*<ParentComp />
+      <FocusInput />
+      {/*<RefsDemo />
+      <ParentComp />
       <Table />
       <FragmentDemo />
        <LifecycleA />
